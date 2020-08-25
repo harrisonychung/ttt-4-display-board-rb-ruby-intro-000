858ff3d8370babb3cb14fa5dcc22994d8bec3a74
Harrison Chung <harrison.y.chung@gmail.com>
2020-08-25 18:32:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,6 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts " #{board[0]} + " " | #{board[1]} | #{board[2]}}"
-  puts " #{board[3]} | #{board[4]} | #{board[5]}}"
+  puts " " + " #{board[3]} + " | #{board[4]} | #{board[5]}}"
   puts " #{board[4]} | #{board[5]} | #{board[6]}}"
   end
